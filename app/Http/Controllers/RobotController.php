e4b924b8cd6511806a3802ece6e732bd27917320
unknown <abdullah.alnahhal@gmail.com>
2021-11-16 00:01:21 +0200

    make files

--- app/Http/Controllers/RobotController.php
+++ app/Http/Controllers/RobotController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Str;
 use App\Services\RobotService;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Request;
 use App\Http\Requests\RobotParseRequest;
+use App\Templates\RobotTemplate;
 
 class RobotController extends Controller
 {
@@ -24,6 +25,23 @@ class RobotController extends Controller
         $this->service = $service;
     }
 
+    private function parseFile(RobotTemplate $robot)
+    {
+        $text = "Final Position : \n ------ \n";
+        $text .= "X = ".$robot->position['x'].", Y = ".$robot->position['y']."\n ------ \n";
+        $text .= "\nFinal Position : \n ------ \n".implode(', ', $robot->literal_history)."\n ------ \n";
+
+        $counter = 0;
+
+        $text .= "\nFull History : \n ------ \n";
+
+        foreach($robot->history as $instruction){
+            $text .= $counter + 1 .") X = ".$instruction['x'].", Y = ".$instruction['y']."\n";
+        }
+
+        Storage::disk('public')->put('positions/position-'.uniqid().'.txt', $text);
+    }
+
     /**
      * [parse parsing the instructions]
      *
@@ -34,9 +52,12 @@ class RobotController extends Controller
     {
         $instructions = strtolower($request->instructions);
         $robot = $this->service->parse($instructions);
+        $this->parseFile($robot);
+        $old_files = Storage::disk('public')->allFiles('positions');
         return view('welcome', [
             'robot' => $robot,
-            'instructions' => $request->instructions
+            'instructions' => $request->instructions,
+            'old_files' => $old_files
         ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,14 +16,26 @@
         </div>
 
         <div class='row mt-5'>
-            <div class="col-12 bg-primary p-3 text-white">
+            <div class="col-6 bg-primary p-3 text-white">
                 {{ __('Full History') }}
             </div>
-            <div class="col-12 row p-3 bg-white">
+            <div class="col-6 row p-3 bg-white">
                 @foreach($robot->history as $instruction)
                     <div class='col-md-4 mt-2'>
                         {{ $loop->index + 1 }} ) X = {{ $instruction['x'] }}, Y = {{ $instruction['y'] }}
                     </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div class='row mt-5'>
+            <div class="col-6 bg-primary p-3 text-white">
+                {{ __('Instructions File List') }}
+            </div>
+            <div class="col-6 row p-3 bg-white">
+                @foreach($old_files as $file)
+                    <a href={{ asset('storage/'.$file) }}> {{ $loop->index + 1 }} ) {{ $file }}</a>
+                    <br><br>
                 @endforeach
             </div>
         </div>
